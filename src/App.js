caff1910488be53b10c5ac980d5533f461343db7
Alex Garcia <alejandragviana@gmail.com>
2019-01-14 11:55:17 +0100

    skills test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import MainHome from './components/MainCreator';
-import Footer from './components/Footer';
-// import Home from './components/Home';
 import Creator from './components/Creator';
-//import 'core/_variables.scss';
 import './App.scss';
 
-const arraySkills = ['html', 'css', 'javascript'];
 
 class App extends Component {
-
+  
   constructor (props){
     super(props);
-    this.props = {arraySkills};
+    
+    this.state = {
+      
+    arraySkills : ['html', 'css', 'javascript']
+    };
+    
   }
 
-
   render() {
+
     return (
-          <Creator arraySkills={arraySkills}/>
+          <Creator arraySkills={this.state.arraySkills}/>
     );
   }
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 
 class Skills extends Component {
     render() {
-        const arraySkills = this.props.arraySkills.map((skill, index)) => {
+        const arraySkills = this.props.arraySkills.map((skill, index) => {
             return (
-                console.log (index);
+                <li>{skill}</li>
             );
         }
+    )
         return (
             <React.Fragment>
             <p className="text--skills">Habilidades (máximo 3)</p>
             <div className="container--skills">{arraySkills}</div>
             </React.Fragment>
         );
+        
     }
 }
 
