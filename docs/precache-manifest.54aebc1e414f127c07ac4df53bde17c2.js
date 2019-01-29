1c1d1ab207f617d639b96e2c78389954f817dd39
pixelvega <pixelvega.info@gmail.com>
2019-01-29 13:59:53 +0100

    docs

---
+++ docs/precache-manifest.54aebc1e414f127c07ac4df53bde17c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01799ca5cae13aca5f935e81b831085a",
+    "url": "./static/media/tarjetas-molonas.01799ca5.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7479179b7297d4457101",
+    "url": "./static/js/main.7479179b.chunk.js"
+  },
+  {
+    "revision": "5493d70ad9a6ba7e1388",
+    "url": "./static/js/1.5493d70a.chunk.js"
+  },
+  {
+    "revision": "7479179b7297d4457101",
+    "url": "./static/css/main.c661143a.chunk.css"
+  },
+  {
+    "revision": "3d52024c75e2725a99e480d46e438804",
+    "url": "./index.html"
+  }
+];
